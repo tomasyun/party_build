597bcc1f63743a5c3b4b4b8922fa782fc2026dbb
yunt <yuntuo19900228@126.com>
2019-01-23 11:32:04 +0800

    "考试---已考" 实现数据远程获取

--- lib/item/exam_ok_item.dart
+++ lib/item/exam_ok_item.dart
@@ -15,40 +15,86 @@ class ExamOkItem extends StatelessWidget {
       child: Column(
         children: <Widget>[
           Container(
+            padding: EdgeInsets.all(15.0),
             child: Row(
+              mainAxisAlignment: MainAxisAlignment.end,
               children: <Widget>[
-                Container(
-                  child: Column(
-                    children: <Widget>[
-                      Container(
-                        child: Text("十九大会议精神的测试"),
-                      ),
-                      Container(
-                        child: Row(
-                          children: <Widget>[
-                            Container(
-                              child: Text("2018-11-11"),
-                            ),
-                            Container(
-                              child: Text("已结束"),
-                            )
-                          ],
+                Expanded(
+                  child: Container(
+                    child: Column(
+                      children: <Widget>[
+                        Container(
+                          child: Text(
+                            exam.title,
+                            style:
+                                TextStyle(fontSize: 16.0, color: Colors.black),
+                          ),
                         ),
-                      )
-                    ],
+                        Container(
+                          margin: EdgeInsets.only(top: 20.0),
+                          child: Row(
+                            children: <Widget>[
+                              Container(
+                                child: Text(
+                                  exam.limitDate,
+                                  style: TextStyle(
+                                      fontSize: 14.0, color: Colors.black45),
+                                ),
+                                margin: EdgeInsets.only(top: 3.0),
+                              ),
+                              Container(
+                                padding: EdgeInsets.only(left: 10.0),
+                                child: Text(
+                                  "已结束",
+                                  style: TextStyle(
+                                      fontSize: 14.0, color: Colors.black45),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                    ),
                   ),
+                  flex: 1,
                 ),
-                Container(
-                  padding: EdgeInsets.all(30.0),
-                  decoration: BoxDecoration(
-                      shape: BoxShape.circle, color: Colors.green),
-                  child: Text("80分"),
-                )
+                _buildScoreContainer(
+                    exam.examScore.split(".")[0] + "分", exam.isPass)
               ],
             ),
+          ),
+          Container(
+            width: double.infinity,
+            height: 0.5,
+            color: Colors.black12,
           )
         ],
       ),
     );
+  }
+
+  Widget _buildScoreContainer(String score, String isPass) {
+    if (isPass == "0") {
+      return Container(
+          padding: EdgeInsets.all(30.0),
+          margin: EdgeInsets.only(left: 10.0),
+          decoration: BoxDecoration(shape: BoxShape.circle, color: Colors.red),
+          child: Text(
+            score,
+            style: TextStyle(fontSize: 16.0, color: Colors.white),
+          ));
+    } else if (isPass == "1") {
+      return Container(
+          padding: EdgeInsets.all(30.0),
+          decoration:
+              BoxDecoration(shape: BoxShape.circle, color: Colors.green),
+          child: Text(
+            score,
+            style: TextStyle(fontSize: 16.0, color: Colors.white),
+          ));
+    } else {
+      return null;
+    }
   }
 }
